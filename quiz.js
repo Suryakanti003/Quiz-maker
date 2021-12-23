d01e3587670844ce821fcfb2dcc9472eaaea95b5
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-12-23 22:07:29 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,5 +1,7 @@
+let newTopic = document.getElementById("topic_new");
 function sketch(){
   createCanvas(400,400);
+  newTopic.addEventListener("onClick",()=>console.log("this is working!!"));
 }
 
 function draw(){
