cfbf1506972f7381024e8489dc2057563ec8d88d
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-12-23 22:38:27 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,12 +1,19 @@
 let topics= []
 let newTopic = document.getElementById("topic_new");
+let topicName = document.getElementById("topic_name");
 function setup(){
   createCanvas(400,400);
-  newTopic.addEventListener("click",()=>{console.log("this is working!!");
-                                           let topic = {questions:[],name:""};
-                                           topics.push(topic);});
+  newTopic.addEventListener("click", makeNewTopic());
 }
 
 function draw(){
   background(127);
+}
+
+function makeNewTopic(){
+  console.log("this is finally working");
+  if(topicName.value){
+    let topic = {questions:[],
+                 name = topicName.value}
+  }else alert("Please put a topic name");
 }
