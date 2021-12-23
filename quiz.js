df412b28da85d151c22bb0200b7754d13cb504cf
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-12-23 22:20:43 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,7 +1,10 @@
+let topics= []
 let newTopic = document.getElementById("topic_new");
 function setup(){
   createCanvas(400,400);
-  newTopic.addEventListener("onClick",()=>console.log("this is working!!"));
+  newTopic.addEventListener("onClick",()=>{console.log("this is working!!");
+                                           let topic = {questions:[],name:""};
+                                           topics.push(topic);});
 }
 
 function draw(){
