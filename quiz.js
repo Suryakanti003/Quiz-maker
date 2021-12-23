a652ce066bb94aa514504c65a5290e40edd1879b
Suryakanti003 <68381666+Suryakanti003@users.noreply.github.com>
2021-12-23 22:23:43 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ let topics= []
 let newTopic = document.getElementById("topic_new");
 function setup(){
   createCanvas(400,400);
-  newTopic.addEventListener("onClick",()=>{console.log("this is working!!");
+  newTopic.addEventListener("click",()=>{console.log("this is working!!");
                                            let topic = {questions:[],name:""};
                                            topics.push(topic);});
 }
